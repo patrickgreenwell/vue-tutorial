cbd3b099cf27dee1596ea444ea21fbf60b375946
patrick.greenwell <patrick.greenwell@quickpivot.com>
2019-01-30 02:46:08 -0500

    got to 36:22 in the vue tutorial in https://www.youtube.com/watch?v=78tNYZUS-ps

---
+++ src/components/Skills.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <div class="holder">
+      <ul>
+        <li v-for="(data,index) in skills" :key="index">{{index }}. {{data.skill}}</li>
+      </ul>
+    <div v-bind:style="{ backgroundColor: bgColor, width: bgWidth, height: bgHeight}"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Skills",
+  data() {
+    return {
+      skills: [{ skill: "Vue.js" }, { skill: "Frontend Dev" }],
+      bgColor: 'yellow',
+      bgWidth: '100%',
+      bgHeight: '30px'
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .alert {
+    background-color: yellow;
+    width: 100%;
+    height: 30px;
+  }
+</style>
